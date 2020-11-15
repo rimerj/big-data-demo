ebe6eaff85641e782bd99577f52870ac55b3c00d
Jeremy Rimer <jrimer@brighthealthplan.com>
2020-11-15 10:16:23 -0600

    updated local run settings

--- demo-loader/runit.bat
+++ demo-loader/runit.bat
@@ -6,4 +6,4 @@ echo "Removed .\test_output"
 REM Detect name of JAR in targets
 for /F %%x IN ('cmd /C dir /b target\*depend*') DO set JAR=%%x
 echo "Jar NAME: %JAR%"
-spark-submit --class com.bhp.dp.demo.DemoLoader target\%JAR% -source_path "./mnt/billing/GDP001/runlog/CP10006a/" -target_path "./test_output/"
+spark-submit --class com.bhp.dp.demo.DemoLoader target\%JAR% -target "./test_output/"
